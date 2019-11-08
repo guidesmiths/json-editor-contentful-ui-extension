1a661f71131c72b496d0ebb4d040b87cf338efac
Íñigo Marquínez <inigo.marquinez@guidesmiths.com>
2019-11-08 13:48:37 +0100

    Added full options to JSONEditor. Split create and initialize editor. Testing AJAX requests for dynamic schemas

--- src/Extension.js
+++ src/Extension.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 import schema from './schema.json';
 
-function debounce (func, wait) {
+function debounce(func, wait) {
   let timeout;
   return function () {
-      const context = this, args = arguments;
-      const later = function () {
-          timeout = null;
-          func.apply(context, args);
-      };
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (!timeout) func.apply(context, args);
+    const context = this, args = arguments;
+    const later = function () {
+      timeout = null;
+      func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (!timeout) func.apply(context, args);
   };
 }
 
@@ -44,39 +44,72 @@ const Extension = ({ sdk }) => {
     };
   }, [detachExternalChangeHandler, jsonEditor, sdk.field, sdk.window]);
 
-  const initializeEditor = useCallback(() => {
+  const createEditor = useCallback(() => {
+    const inputSchema = sdk.entry.fields["schema"].getValue();
+
     const editorRef = new JSONEditor(ref.current, { // eslint-disable-line no-undef
-      theme: 'foundation5',
-      schema: schema,
-      no_additional_properties: true,
-      required_by_default: true,
-      startval: value,
+      // ajax: true,
+      // ajaxBase: 'https://github.com/guidesmiths/json-editor-contentful-ui-extension/blob/master/src/', //'https://json-schema.org/learn/examples/',
+      compact: false,
+      disable_array_add: false,
+      disable_array_delete: false,
+      disable_array_reorder: false,
+      enable_array_copy: false,
       disable_collapse: true,
+      disable_edit_json: true,
       disable_properties: true,
-      show_errors: 'always'
+      array_controls_top: true,
+      form_name_root: 'root',
+      iconlib: null,
+      remove_button_labels: false,
+      no_additional_properties: true,
+      // refs	An object containing schema definitions for URLs. Allows you to pre-define external schemas.	{}
+      required_by_default: true,
+      keep_oneof_values: true,
+      schema: schema,
+      // schema: {
+      //   $ref: inputSchema, // 'geographical-location.schema.json',
+      // },
+      show_errors: 'always', // interaction, change, always, never
+      startval: value,
+      template: 'default',
+      theme: 'foundation5',
+      display_required_only: false,
+      show_opt_in: true,
+      prompt_before_delete: false,
     });
     setJSONEditor(editorRef);
 
+    console.log('editor', editorRef);
+  }, [ref, sdk.entry.fields]);
+
+  const initializeEditor = useCallback(() => {
     const watcherCallback = () => {
       sdk.window.updateHeight();
       validateAndSave();
     };
 
-    Object.keys(editorRef.editors).forEach(key => {
-      if (Object.prototype.hasOwnProperty.call(editorRef.editors, key) && key !== 'root') {
-        editorRef.watch(key, watcherCallback.bind(editorRef, key));
+    console.log('KEYS', jsonEditor.editors);
+    Object.keys(jsonEditor.editors).forEach(key => {
+      console.log('Key', key);
+      if (Object.prototype.hasOwnProperty.call(jsonEditor.editors, key) && key !== 'root') {
+        jsonEditor.watch(key, watcherCallback.bind(jsonEditor, key));
       }
     });
 
     const validateAndSave = debounce(function () {
-      const errors = editorRef.validate();
-      if (errors.length === 0) sdk.field.setValue(editorRef.getValue());
+      const errors = jsonEditor.validate();
+      if (errors.length === 0) sdk.field.setValue(jsonEditor.getValue());
     }, 150);
-  }, [ref, sdk.field, sdk.window, value]);
+  }, [jsonEditor, sdk.field, sdk.window]);
 
   useEffect(() => {
-    if (ref && !jsonEditor) initializeEditor();
-  }, [ref, jsonEditor, initializeEditor]);
+    if (ref && !jsonEditor) createEditor();
+  }, [ref, jsonEditor, createEditor]);
+
+  useEffect(() => {
+    if (jsonEditor) initializeEditor();
+  }, [jsonEditor, initializeEditor]);
 
   return <div ref={ref} />;
 }
